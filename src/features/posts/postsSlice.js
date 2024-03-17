91645bed7f9cf18da5a48cb12c9b2115c8378745
mintaekim <kmintae33@gmail.com>
2024-03-17 23:58:14 +0800

    export postAdded reducer

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -17,9 +17,16 @@ const initialState = [
 const postsSlice = createSlice({
   name: "posts",
   initialState,
-  reducers: {},
+  reducers: {
+    // create a reducer function to handle the data that we submit
+    possAdded(state, action) {
+      state.push(action.payload);
+    },
+  },
 });
 
 export const selectAllPosts = (state) => state.posts;
+
+export const { postAdded } = postsSlice.actions;
 
 export default postsSlice.reducer;
